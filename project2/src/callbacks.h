404d50f13cbed5c6f5aaaa3e0c48804acdec6f43
SnapeV1 <hamada_amine@yahoo.com>
2022-12-01 06:13:40 -0800

    adding search function

---
+++ project2/src/callbacks.h
@@ -0,0 +1,126 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button_Ajouter_clicked              (GtkButton       *objet,
+                                        gpointer         user_data);
+
+void
+on_button_gestionU_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_gestionE_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_gestionBV_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_ModifierU_clicked            (GtkButton       *objet,
+                                        gpointer         user_data);
+
+void
+on_button_ajouterbv_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_modifierbv_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_Supprimerbv_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_AjouterSlim_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_ModiferSlim_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_SypprimerSlim_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button11_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button13_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button20_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button22_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on__clicked                            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button23_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button24_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button25_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button32_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_hommeU_toggled          (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_femmeU_toggled          (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_checkbutton13_toggled               (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_affichageTree_row_activated         (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_AfficherU_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_affichageTree_row_activated         (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_ModiferUser_clicked                 (GtkButton       *objet,
+                                        gpointer         user_data);
+
+void
+on_button_SupprimerU_clicked           (GtkButton       *objet,
+                                        gpointer         user_data);
+
+void
+on_refresh_clicked                     (GtkButton       *objet,
+                                        gpointer         user_data);
+
+void
+on_button_rechercher_clicked           (GtkButton       *objet,
+                                        gpointer         user_data);
